2991245a135c6eb07dab1c815a409f17559974c7
Zachary Rais-Norman <hey@zacme.ink>
2021-04-05 13:35:42 -0400

    initial commit, basic componenet structure

---
+++ src/lib/Task.svelte
@@ -0,0 +1,39 @@
+<!-- TODO: 
+  add an animation into done 
+  add a drop shadow while grabbed (other styles?)
+-->
+<script>
+	export let done;
+	export let text;
+</script>
+
+<div class:done>This is the text of the task</div>
+
+<style>
+	div {
+		height: 40px;
+		width: 220px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 2px solid #ff3e00;
+		border-radius: 8px;
+		padding: 2px 20px 4px 20px;
+		margin: 10px;
+		overflow: hidden;
+		transition: background-color 0.2s;
+	}
+
+	.done {
+		background-color: hsl(15, 100%, 90%);
+		text-decoration: line-through wavy #ff3e00;
+	}
+
+	.done:hover {
+		background-color: hsl(15, 100%, 90%);
+	}
+
+	div:hover {
+		background-color: hsl(15, 100%, 98%);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,78 @@
+<script>
+	import Counter from '$lib/Counter.svelte';
+	import Task from '$lib/Task.svelte';
+
+	let todos = [
+		{ id: 1, done: false, description: 'write some docs' },
+		{ id: 2, done: false, description: 'start writing JSConf talk' },
+		{ id: 3, done: true, description: 'buy some milk' },
+		{ id: 4, done: false, description: 'mow the lawn' },
+		{ id: 5, done: false, description: 'feed the turtle' },
+		{ id: 6, done: false, description: 'fix some bugs' }
+	];
+
+	let uid = todos.length + 1;
+
+	function add(input) {
+		const todo = {
+			id: uid++,
+			done: false,
+			description: input.value
+		};
+
+		todos = [todo, ...todos];
+		input.value = '';
+	}
+
+	function remove(todo) {
+		todos = todos.filter((t) => t !== todo);
+	}
+</script>
+
+<main>
+	<h1>Hello world!</h1>
+
+	<Task />
+	<Task done />
+	<Task />
+
+	<input
+		class="new-todo"
+		placeholder="ugh, what now?"
+		on:keydown={(event) => event.key === 'Enter' && add(event.target)}
+	/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 100;
+		line-height: 1.1;
+		margin: 4rem auto;
+		max-width: 14rem;
+	}
+
+	p {
+		max-width: 14rem;
+		margin: 2rem auto;
+		line-height: 1.35;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			max-width: none;
+		}
+
+		p {
+			max-width: none;
+		}
+	}
+</style>
